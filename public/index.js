e838ad071a338b4a1fe6a8dd1f685126a8092322
Jason Reed <jreed@chef.io>
2016-01-01 21:47:25 -0500

    playhead

--- public/index.js
+++ public/index.js
@@ -1,4 +1,4 @@
-var song_data = "5sbk2l00e0ft8a7g0fjfi1r1w11312511f10300100d10111011c01000000h06000000v00000000o3210b28p2CexaBIqVY0y28p2CewW4GmM8wy6gFzEexaBI6cwNA688ix248wp2fCkRYzj7gFwqpa1AGGGRoNVVQv49M6SSG0wqrrV5TI-hA96hB86GKNPsW-0wo5ceGsGCGFjhWOqBcc30FDNKTOcz8kRJtc3vlltvlll0RlSnLi60RSSRlSmOs2W1Dp-0U31gk2FNBRnMMs8kMdcD0OtlluIpssqny4U3rrl0gddJYzVQPDOcx8OcF0RlSqCJNWmVY30gc3lllllGsq6inBl-6xX7DkpahAp2CLHFwoeGGW-GGG1GHIL-Qc1HJJGHIJAU5Y05eceePZ1M62wE5e-256VxwxwwGGxeoYjgsyq8llgaGEmll1OGE2e1Q854Qb8CwVlkgGGwllgIyq3Ajg4tUtwFGxpdk7aGy5lk2CG5ARgsGG0xpxy99xPCu2GNmpApfwGElGejyqgaX59_ApQwFY2fvE85lkbyGEehd4a9E54Qb8CwVcQ179q62GG5Ajgsyq8lrgaGEmjl1OGE2CvCg422GG5IjgsyqjgWOaE5lkbqGwVlk1UX8XMkjgJyq3Bln5p5k2GG5Ijgsyq0Y1p0twFGxrdk7aGKaOaE5dkbpGwVlk1j74WxbKsPMlmCnKrKNA-2GJPehSsji1nuZyMmNwcwFI3U1tangaGEntlgsyqYaOqE54QboCwVcQ1U1NjG1ll2S9EbCOAjg0FzE4okzihF8Jh6IzihF8IIhx2d96AyO96MzshI8IyhI8QAqib9Ao8yDNYB6CzZF1a9DSC7ln5YllNv5lv2v0YDMgsllNv5lsnNlnMJMgim3RVtunUMY3VAHw-9OC3UBbp-9uHpYBwRunnBYOof9qfNliuf91N5v12P3GGY05cjAwdj0H04g8OczydDqPIgJESdLzcTycz8UzpSITdJEkP89EaYsNRdt5dd5dbB0PeoEWT8Uz8ESIyyzo1NmNhNmhN6hhJp553Isg3bOasHoEUD8EES0snIkslArRulhKpRRzN6TNSrN6hAshIr6hAkkr6NN6hN6hAshIXmrCSQ0000";
+var song_data = "5sbk3l00e0ft1a7g0fj7i0r0w5211f1130d1111c1100h0000v0011o3410b000id5pUlDwid18Q4zgid18Q4zgid5pU4hgh528z8ycp278FHyYBwpf1imFd4GOCmhFD-1ceo6CgjiQgpllc7j71FJAugJJd7IzOGqqH86CGKOWgUhOCosmCDPSWWo2KOSW-1FGVFjb0g3jbxc_g9xP0QOaqnocGGC3FzXj9mswXiqfF7BnQSTg5dltBIBMxwbceYXCHKmkRlSmTnM5diqvassrKQO0kNUFj3w6jjA0Pon81AQV0e9gCg39FO0oLtA0Oqsw74Ef81AQV0clKO0pdeg3CSV0cCD8190wMp6i39O0qCopgNV49i3rOqI2PJ9LObqgrujlwv67ihv8wWgrujlwecKAH1AmQwSYCH0Kc6Ay-hni3rOqI_6rilwObqgrujl1QpyTBYiFEwXcORcee9SpeyWKlUzbMyIPWoD3j0AxAzOi79M450cD9i0z81IV8a0peiO0Csw7pOgk0OsDycw6PAwE1JQkpk1tSxVFewd0q6o4pj80"; // himenepit
 
 $(go);
 
@@ -64,7 +64,9 @@ function go() {
       processData: false,
       data: song_data,
       success: function(data, status, req) {
-        startPlayback(getAgenda(data));
+        window.parsed_data = data;
+        render(data);
+        startPlayback(data, getAgenda(data));
       },
       error: function (req, status, err) {
         console.log(err);
@@ -75,7 +77,7 @@ function go() {
 }
 
 var PAT_WIDTH = 50;
-var w_scale = PAT_WIDTH / 32;
+
 var PAT_HEIGHT = 80;
 
 function stopBeeps() {
@@ -83,7 +85,9 @@ function stopBeeps() {
   rem.send("halt", {});
 }
 
-function render(data) {
+// playhead in beats
+function render(data, playhead) {
+  var w_scale = PAT_WIDTH / beatsPerBar(data);
   var c = $("#c")[0];
   var d = c.getContext('2d');
   var w = c.width = 1000;
@@ -112,40 +116,42 @@ function render(data) {
       }
     }
   }
+  if (playhead != null) {
+    var xpos = playhead * w_scale;
+    d.beginPath();
+    d.moveTo(xpos,0);
+    d.lineTo(xpos, h);
+    d.strokeStyle = "white";
+    d.lineWidth = 2;
+    d.stroke();
+  }
+}
+
+function beatsPerBar(data) {
+  return data.parts * data.beats;
 }
 
 function getAgenda(data) {
   var id_odom = 0;
   var agenda = [];
 
-  var c = $("#c")[0];
-  var d = c.getContext('2d');
-  var w = c.width = 1000;
-  var h = c.height = 500;
-  d.fillRect(0,0,w,h);
   for (var chan = 0; chan < data.channelBars.length; chan++) {
     var bars = data.channelBars[chan];
     for (var bar = 0; bar < bars.length; bar++) {
-      d.fillStyle = "white";
-      d.strokeStyle = "blue";
-      d.lineWidth = 1;
-      d.fillText(bars[bar], bar * PAT_WIDTH + 5, chan * PAT_HEIGHT + 15);
-      d.strokeRect(bar * PAT_WIDTH, chan * PAT_HEIGHT, PAT_WIDTH, PAT_HEIGHT);
       if (bars[bar] > 0) {
-        d.fillStyle = ["red", "yellow", "green", "blue"][chan];
         data.channelPatterns[chan][bars[bar]-1].tones.forEach(function(tone) {
           tone.notes.forEach(function(note) {
             if (chan != 3) {
-              agenda.push([bar * 32 + tone.start,
+              agenda.push([bar * beatsPerBar(data) + tone.start,
                            {action: "note",
                             args: {
                               on: true,
                               id: id_odom,
-                              ugenName: "lead",
+                              ugenName: chan == 2 ? "lead" : "midi",
                               vel: 10,
-                              pitch: note + 24,
+                              pitch: note + 12,
                             }}]);
-              agenda.push([bar * 32 + tone.end,
+              agenda.push([bar * beatsPerBar(data) + tone.end,
                            {action: "note",
                             args: {
                               on: false,
@@ -153,12 +159,6 @@ function getAgenda(data) {
                             }}]);
               id_odom++;
             }
-            d.fillRect(
-              bar * PAT_WIDTH + tone.start * w_scale,
-              chan * PAT_HEIGHT + 75 - note,
-              (tone.end - tone.start) * w_scale,
-              2
-            );
           });
         });
       }
@@ -170,13 +170,14 @@ function getAgenda(data) {
 function stopPlayback() {
 
 }
-function startPlayback(agenda) {
+function startPlayback(songdata, agenda) {
   window.playing = true;
-  var beatSamples = 0.075 * 44100;
+  var beatSamples = 0.15 * 44100;
   var play_head = 0; // samples
   var i = 0; // position in agenda
   var server_time = 0; // samples
   function play_a_bit() {
+    render(songdata, (play_head - server_time) / beatSamples );
     var cmds = [];
     for (; i < agenda.length && server_time + agenda[i][0] * beatSamples < play_head + 10000; i++) {
       cmds.push({time: Math.floor(server_time + agenda[i][0] * beatSamples),
